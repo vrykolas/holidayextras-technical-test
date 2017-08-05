7ecebee40b7bbb9f1a80e08aa38b2d212e247192
Scott Timmins <scotttimmins@hotmail.com>
2017-08-05 23:57:26 +0100

    test: add update userr tests

---
+++ test/users/update.js
@@ -0,0 +1,83 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+chai.should();
+
+const request = chai.request(require('../../src/app'));
+
+describe('User Update', () => {
+  it('Should warn if a user does not exist', (done) => {
+    request.put('/users/999999999999999')
+      .then((res) => {
+        done(new Error('Invalid response'));
+      })
+      .catch(() => {
+        done();
+      });
+  });
+
+  it('Should update a valid user', (done) => {
+    const user = {
+      email: 'bob6@example.com',
+      forename: 'Bob',
+      surname: 'Bobson'
+    };
+
+    const updatedUser = {
+      email: 'tom@example.com',
+      forename: 'Tom',
+      surname: 'Tomson'
+    };
+
+    let userId;
+
+    request.post('/users')
+      .send(user)
+      .then((res) => {
+        userId = res.body.id;
+        return request.put(`/users/${userId}`).send(updatedUser);
+      })
+      .then(() => {
+        return request.get(`/users/${userId}`);
+      })
+      .then((res) => {
+        res.should.have.status(200);
+        res.body.email.should.eql(updatedUser.email);
+        res.body.forename.should.eql(updatedUser.forename);
+        res.body.surname.should.eql(updatedUser.surname);
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  it('Should be unable to update a user to a duplicate email', (done) => {
+    const user1 = {
+      email: 'bob7@example.com',
+      forename: 'Bob',
+      surname: 'Bobson'
+    };
+    const user2 = {
+      email: 'bob8@example.com',
+      forename: 'Bob',
+      surname: 'Bobson'
+    };
+
+    request.post('/users')
+      .send(user1)
+      .then((res) => {
+        return request.post('/users').send(user2);
+      })
+      .then((res) => {
+        return request.put(`/users/${res.body.id}`).send(user1);
+      })
+      .then((res) => {
+        done(new Error('Invalid response'));
+      })
+      .catch(() => {
+        done();
+      });
+  });
+});
